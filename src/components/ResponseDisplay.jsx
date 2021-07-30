360a6b6171417dcc5735f1dc4adf3c8ff4913268
cmmerritt <christiane.merritt@gmail.com>
2021-07-30 16:54:54 -0700

    add response display area

---
+++ src/components/ResponseDisplay.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ResponseDisplay = () => {
+  return <p>JSON response here</p>
+};
+
+export default ResponseDisplay;
--- src/containers/PostPersonContainer.jsx
+++ src/containers/PostPersonContainer.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Fetcher from '../components/Fetcher';
 import Header from '../components/Header';
+import ResponseDisplay from '../components/ResponseDisplay';
 import Sidebar from '../components/SidebarItem';
 
 export default class PostPersonContainer extends Component {
@@ -10,6 +11,7 @@ export default class PostPersonContainer extends Component {
         <header><Header /></header>
         <p><Sidebar /></p>
         <p><Fetcher /></p>
+        <p><ResponseDisplay /></p>
       </>
     );
   }
