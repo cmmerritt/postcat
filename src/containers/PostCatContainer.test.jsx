e0e37fc62c85a71045d31f8a94fd022a2b2f8df6
cmmerritt <christiane.merritt@gmail.com>
2021-08-03 11:47:28 -0700

    URL input test passes

--- src/containers/PostCatContainer.test.jsx
+++ src/containers/PostCatContainer.test.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
 import PostCatContainer from './PostCatContainer';
 
 describe('PostCat Container', () => {
   it('renders PostCat', async () => {
-    render(<PostCatContainer />);
+    render(<MemoryRouter><PostCatContainer /></MemoryRouter>);
+
+    // test getting URL user types in
+    const userURL = await screen.findByPlaceholderText('URL');
+    fireEvent.change(userURL,
+      { 
+        target:
+        {
+          value: 'https://rickandmortyapi.com/api/character'
+        }
+      });
+    expect(userURL.value).toBe('https://rickandmortyapi.com/api/character');
+
+    // test getting radio button value
+
+    // test user entering request body text
+
+    // test user clicking submit button
+
+    // test history displaying on sidebar
+
   });
 });
 
