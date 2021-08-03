027f852581850ef1951a493522008e91241ea1e8
cmmerritt <christiane.merritt@gmail.com>
2021-08-03 11:20:37 -0700

    sidebar snapshot test passes

---
+++ src/components/Sidebar.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import Sidebar from '. Sidebar';
+
+describe( 'Sidebar component', () => {
+  afterEach(() => cleanup());
+  it('renders Sidebar', () => {
+    const { asFragment } = render( Sidebar />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
